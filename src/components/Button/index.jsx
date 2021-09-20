c1c72a508adcd0925825ad90c371ff42ff7f7497
cdsnascimento <cdsnascimento@hotmail.com>
2021-09-20 00:30:25 -0300

    76 - Context API + Hooks(unindo idéisas) - parte 6

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,15 @@
 import P from 'prop-types';
 
-export const Button = ({ children, onButtonClick }) => {
-  return <button onClick={onButtonClick}>Increase</button>;
+export const Button = ({ children, onButtonClick, disabled = false }) => {
+  return (
+    <button disabled={disabled} onClick={onButtonClick}>
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
   children: P.node.isRequired,
   onButtonClick: P.func.isRequired,
-}
+  disabled: P.bool,
+};
--- src/pages/Home2/index.jsx
+++ src/pages/Home2/index.jsx
@@ -4,15 +4,37 @@ import { Button } from '../../components/Button';
 import { Heading } from '../../components/Heading';
 import { useCounterContext } from '../../contexts/CounterContext';
 
-
 export const Home2 = () => {
   const [state, actions] = useCounterContext();
+
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then(r => console.log(r))
+      .catch((err) => console.log(err.name, ': ', err.message));
+  };
 
   return (
     <div>
       <Heading />
       <div>
-        <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.increase}>increase</Button>
+
+        <Button onButtonClick={actions.decrease}>decrease</Button>
+
+        <Button onButtonClick={actions.reset}>reset</Button>
+
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>set 10</Button>
+
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>set 100</Button>
+
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          asyncIncrease
+        </Button>
+
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          asyncError
+        </Button>
       </div>
     </div>
   );
